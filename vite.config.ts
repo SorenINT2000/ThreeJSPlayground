2f80593f8937ff6b5361eb4ec462fc1944dededc
Soren Schultz <sjschultz11@gmail.com>
2026-03-07 22:40:33 -0800

    update config

--- vite.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  // Required for GitHub Pages project sites: site is at /ThreeJSPlayground/, not /
-  base: '/ThreeJSPlayground/',
-});
